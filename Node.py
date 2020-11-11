3733edcc03da7831ac9980472dc793bf81754107
Danny Nguyen <dnguyen274@hotmail.com>
2020-11-11 17:55:54 -0500

    Finished uniform cost algorithm and Node class

--- Node.py
+++ Node.py
@@ -5,9 +5,9 @@ from operator import attrgetter
 class Node:
     # Class attribute
     weight = 0
-    def __init__(self, state, old_state, weight):
+    def __init__(self, state, old_Node, weight):
         self.state = state
-        self.old_state = old_state
+        self.old_Node = old_Node
         self.weight = weight
 
 
@@ -31,55 +31,176 @@ class Node:
             isCorner = True
         else: isCorner = False
 
-       #1-Create successor for first move
+       #1-Create successor for regular move
 
         leftPos = (position[0], position[1]-1)
         rightPos = (position[0], position[1] + 1)
         bottomPos = (position[0] + 1, position[1])
         topPos = (position[0] - 1, position[1])
 
-        # initialState.state[0][0], initialState.state[pos[0]][pos[1]] = initialState.state[pos[0]][pos[1]], initialState.state[0][0]
-        # leftNode = Node()
+        successorList = []
+        if leftPos[1] >= 0:
+            leftState = np.copy(self.state)
+            leftState[leftPos[0]][leftPos[1]], leftState[position[0]][position[1]] = leftState[position[0]][position[1]], leftState[leftPos[0]][leftPos[1]]
+            leftNode = Node(leftState, self, self.weight + 1)
+            successorList.append(leftNode)
 
-        if leftPos[1]>=0:
-            tempState = np.array.copy(self.state)
-            tempState.state[leftPos[0]][leftPos[1]], tempState.state[position[0]][position[1]] = tempState.state[position[0]][position[1]],tempState.state[leftPos[0]][leftPos[1]]
-            leftNode = Node()
-        if rightPos[1] <= col:
-            #
-        if bottomPos[0] <=row:
-            #
+        if rightPos[1] <= col-1:
+            rightState = np.copy(self.state)
+            rightState[rightPos[0]][rightPos[1]], rightState[position[0]][position[1]] = \
+            rightState[position[0]][position[1]], rightState[rightPos[0]][rightPos[1]]
+            rightNode = Node(rightState, self, self.weight + 1)
+            successorList.append(rightNode)
+
+        if bottomPos[0] <= row-1:
+            botState = np.copy(self.state)
+            botState[bottomPos[0]][bottomPos[1]], botState[position[0]][position[1]] = \
+            botState[position[0]][position[1]], botState[bottomPos[0]][bottomPos[1]]
+            bottomNode = Node(botState, self, self.weight + 1)
+            successorList.append(bottomNode)
+
         if topPos[0] >= 0:
-            #
+            topState = np.copy(self.state)
+            topState[topPos[0]][topPos[1]], topState[position[0]][position[1]] = \
+            topState[position[0]][position[1]], topState[topPos[0]][topPos[1]]
+            topNode = Node(topState, self, self.weight + 1)
+            successorList.append(topNode)
 
+        if isCorner:
+            # Create Wrapping moves
+            if position[1] == col-1:
+                wrapLeftState = np.copy(self.state)
+                wrapLeftState[position[0]][0], wrapLeftState[position[0]][position[1]] = \
+                    wrapLeftState[position[0]][position[1]], wrapLeftState[position[0]][0]
+                wrapLeftNode = Node(wrapLeftState, self, self.weight + 2)
+                alreadyExists = False
+                for successorNode in successorList:
+                    if np.array_equal(wrapLeftNode.state, successorNode.state):
+                        alreadyExists = True
+                if not alreadyExists:
+                    successorList.append(wrapLeftNode)
 
+            elif position[1] == 0:
+                wrapRightState = np.copy(self.state)
+                wrapRightState[position[0]][col-1], wrapRightState[position[0]][position[1]] = \
+                    wrapRightState[position[0]][position[1]], wrapRightState[position[0]][col-1]
+                wrapRightNode = Node(wrapRightState, self, self.weight + 2)
+                alreadyExists = False
+                for successorNode in successorList:
+                    if np.array_equal(wrapRightNode.state, successorNode.state):
+                        alreadyExists = True
+                if not alreadyExists:
+                    successorList.append(wrapRightNode)
 
+            if position[0] == row-1:
+                wrapTopState = np.copy(self.state)
+                wrapTopState[0][position[1]], wrapTopState[position[0]][position[1]] = \
+                    wrapTopState[position[0]][position[1]], wrapTopState[0][position[1]]
+                wrapTopNode = Node(wrapTopState, self, self.weight + 2)
+                alreadyExists = False
+                for successorNode in successorList:
+                    if np.array_equal(wrapTopNode.state, successorNode.state):
+                        alreadyExists = True
+                if not alreadyExists:
+                    successorList.append(wrapTopNode)
 
+            elif position[0] == 0:
+                wrapBottomState = np.copy(self.state)
+                wrapBottomState[row - 1][position[1]], wrapBottomState[position[0]][position[1]] = \
+                    wrapBottomState[position[0]][position[1]], wrapBottomState[row -1 ][position[1]]
+                wrapBottomNode = Node(wrapBottomState, self, self.weight + 2)
+                alreadyExists = False
+                for successorNode in successorList:
+                    if np.array_equal(wrapBottomNode.state, successorNode.state):
+                        alreadyExists = True
+                if not alreadyExists:
+                    successorList.append(wrapBottomNode)
 
+            # Create Diagonal moves
+            if position == (0, 0):
+                oppositeCornerState = np.copy(self.state)
+                oppositeCornerState[row-1][col-1], oppositeCornerState[0][0] = \
+                oppositeCornerState[0][0], oppositeCornerState[row-1][col-1]
+                oppositeCornerNode = Node(oppositeCornerState, self, self.weight + 3)
+                successorList.append(oppositeCornerNode)
 
+                diagonalState = np.copy(self.state)
+                diagonalState[1][1], diagonalState[0][0] = \
+                    diagonalState[0][0], diagonalState[1][1]
+                diagonalNode = Node(diagonalState, self, self.weight + 3)
+                successorList.append(diagonalNode)
+            elif position == (0, col-1):
+                oppositeCornerState = np.copy(self.state)
+                oppositeCornerState[row - 1][0], oppositeCornerState[0][col-1] = \
+                    oppositeCornerState[0][col-1], oppositeCornerState[row - 1][0]
+                oppositeCornerNode = Node(oppositeCornerState, self, self.weight + 3)
+                successorList.append(oppositeCornerNode)
 
+                diagonalState = np.copy(self.state)
+                diagonalState[1][col-2], diagonalState[0][col-1] = \
+                    diagonalState[0][col-1], diagonalState[1][col-2]
+                diagonalNode = Node(diagonalState, self, self.weight + 3)
+                successorList.append(diagonalNode)
+            elif position == (row-1, 0):
+                oppositeCornerState = np.copy(self.state)
+                oppositeCornerState[0][col - 1], oppositeCornerState[row-1][0] = \
+                    oppositeCornerState[row-1][0], oppositeCornerState[0][col - 1]
+                oppositeCornerNode = Node(oppositeCornerState, self, self.weight + 3)
+                successorList.append(oppositeCornerNode)
 
-        #2 Add successors to openList
+                diagonalState = np.copy(self.state)
+                diagonalState[row-2][1], diagonalState[row-1][0] = \
+                    diagonalState[row-1][0], diagonalState[row-2][1]
+                diagonalNode = Node(diagonalState, self, self.weight + 3)
+                successorList.append(diagonalNode)
+            elif position == (row-1, col-1):
+                oppositeCornerState = np.copy(self.state)
+                oppositeCornerState[row - 1][col - 1], oppositeCornerState[0][0] = \
+                    oppositeCornerState[0][0], oppositeCornerState[row - 1][col - 1]
+                oppositeCornerNode = Node(oppositeCornerState, self, self.weight + 3)
+                successorList.append(oppositeCornerNode)
+
+                diagonalState = np.copy(self.state)
+                diagonalState[row-2][col-2], diagonalState[row-1][col-1] = \
+                    diagonalState[row-1][col-1], diagonalState[row-2][col-2]
+                diagonalNode = Node(diagonalState, self, self.weight + 3)
+                successorList.append(diagonalNode)
+
+        return successorList
+
 
 
 # Parse string to get inital state puzzle
-initialState = Node(np.array([[3, 0, 1, 4], [2, 6, 5, 7]]), None, 0)
+initialState = Node(np.array([[1, 6, 3, 4], [0, 2, 7, 5]]), None, 0)
 
-openList = [initialState, initialState1, initialState2]
+openList = [initialState]
 closedList = []
 
-initialState.createSuccessors()
+while True:
+    # Check if first Node is the goal
+    if openList[0].isGoal():
+        print("Solution found, Total cost: ", openList[0].weight)
+        break
 
-#while True:
-#Sort open list by weight
-openList.sort(key=attrgetter('weight'))
+    # Pop the visited node and add to closed list
+    visitedNode = openList.pop(0)
+    closedList.append(visitedNode)
 
-#Create successor
-openList[0].createSuccessors()
+    #Create successor
+    newSuccessorList = visitedNode.createSuccessors()
+    for successorNode in newSuccessorList:
+        for openNode in openList:
+            if np.array_equal(successorNode.state, openNode.state) and successorNode.weight < openNode.weight:
+                openList.remove(openNode)
+        alreadyExists = False
+        for closedNode in closedList:
+            if np.array_equal(successorNode.state, closedNode.state):
+                alreadyExists = True
+        if not alreadyExists:
+            openList.append(successorNode)
 
-#Pop the visited node and add to closed list
+    # Sort open list by weight
+    openList.sort(key=attrgetter('weight'))
 
-#Check if first Node is the goal
-if openList[0].isGoal():
-    print("Solution found")
+
 
