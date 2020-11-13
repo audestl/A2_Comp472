fab47eb809a8ac3f1ecb110e352dd4ac14816f3d
Danny Nguyen <dnguyen274@hotmail.com>
2020-11-13 18:20:15 -0500

    Added output for solution and search files

--- Node.py
+++ Node.py
@@ -5,18 +5,52 @@ from operator import attrgetter
 class Node:
     # Class attribute
     weight = 0
-    def __init__(self, state, old_Node, weight):
+    # Constructor used for A*
+    def __init__(self, state, old_Node, tileMoved, moveCost, Gn):
         self.state = state
         self.old_Node = old_Node
-        self.weight = weight
+        self.tileMoved = tileMoved
+        self.moveCost = moveCost
+        self.weight = Gn
+        # self.Hn = computeHn(self)
+        # self.Fn = Gn + self.Hn
 
+    # Constructor used for Uniform Cost (the input parameter estimatedWeight is always coded to 0)
+    def __init__(self, state, old_Node, tileMoved, moveCost,Gn, Hn):
+        self.state = state
+        self.old_Node = old_Node
+        self.tileMoved = tileMoved
+        self.moveCost = moveCost
+        self.weight = Gn
+        self.Hn = Hn
+        self.Fn = Gn + Hn
 
+    # def computeHn(self):
+    #     goalCost1 = 0
+    #     goalCost2 = 0
+    #     maxRow = len(self.state) - 1
+    #     maxCol = len(self.state[0]) - 1
+
+    def solutionToString(self):
+        str1 = ""
+        if self.old_Node != None:
+            str1 += self.old_Node.solutionToString()
+        str1 += str(self.tileMoved) + " " + str(self.moveCost) + " " + self.toString() + "\n"
+        return str1
+    
     def isGoal(self):
         if np.array_equal(self.state, np.array([[1,2,3,4], [5,6,7,0]])) or np.array_equal(self.state, np.array([[1,3,5,7], [2,4,6,0]])):
             return True
         else: return False
 
-    def createSuccessors(self):
+    def toString(self):
+        str1 = ""
+        for row in self.state:
+            for number in row:
+                str1 += str(number) + " "
+        return str1
+
+    def createSuccessors(self, isUniformCost):
         #Calculte position
         row = len(self.state)
         col = len(self.state[0])
@@ -42,28 +76,32 @@ class Node:
         if leftPos[1] >= 0:
             leftState = np.copy(self.state)
             leftState[leftPos[0]][leftPos[1]], leftState[position[0]][position[1]] = leftState[position[0]][position[1]], leftState[leftPos[0]][leftPos[1]]
-            leftNode = Node(leftState, self, self.weight + 1)
+            leftNode = Node(leftState, self, leftState[position[0]][position[1]], 1, self.weight + 1, 0) if isUniformCost \
+                else Node(leftState, self,  leftState[position[0]][position[1]], 1, self.weight + 1)
             successorList.append(leftNode)
 
         if rightPos[1] <= col-1:
             rightState = np.copy(self.state)
             rightState[rightPos[0]][rightPos[1]], rightState[position[0]][position[1]] = \
             rightState[position[0]][position[1]], rightState[rightPos[0]][rightPos[1]]
-            rightNode = Node(rightState, self, self.weight + 1)
+            rightNode = Node(rightState, self,  rightState[position[0]][position[1]], 1, self.weight + 1, 0) if isUniformCost \
+                else Node(rightState, self, rightState[position[0]][position[1]], 1, self.weight + 1)
             successorList.append(rightNode)
 
         if bottomPos[0] <= row-1:
             botState = np.copy(self.state)
             botState[bottomPos[0]][bottomPos[1]], botState[position[0]][position[1]] = \
             botState[position[0]][position[1]], botState[bottomPos[0]][bottomPos[1]]
-            bottomNode = Node(botState, self, self.weight + 1)
+            bottomNode = Node(botState, self,  botState[position[0]][position[1]], 1, self.weight + 1, 0) if isUniformCost \
+                else Node(botState, self, botState[position[0]][position[1]], 1, self.weight + 1)
             successorList.append(bottomNode)
 
         if topPos[0] >= 0:
             topState = np.copy(self.state)
             topState[topPos[0]][topPos[1]], topState[position[0]][position[1]] = \
             topState[position[0]][position[1]], topState[topPos[0]][topPos[1]]
-            topNode = Node(topState, self, self.weight + 1)
+            topNode = Node(topState, self,  topState[position[0]][position[1]], 1, self.weight + 1, 0) if isUniformCost \
+                else Node(topState, self, topState[position[0]][position[1]], 1, self.weight + 1)
             successorList.append(topNode)
 
         if isCorner:
@@ -72,7 +110,8 @@ class Node:
                 wrapLeftState = np.copy(self.state)
                 wrapLeftState[position[0]][0], wrapLeftState[position[0]][position[1]] = \
                     wrapLeftState[position[0]][position[1]], wrapLeftState[position[0]][0]
-                wrapLeftNode = Node(wrapLeftState, self, self.weight + 2)
+                wrapLeftNode = Node(wrapLeftState, self, wrapLeftState[position[0]][position[1]], 2, self.weight + 2, 0) if isUniformCost \
+                    else Node(wrapLeftState, self, wrapLeftState[position[0]][position[1]], 2, self.weight + 2)
                 alreadyExists = False
                 for successorNode in successorList:
                     if np.array_equal(wrapLeftNode.state, successorNode.state):
@@ -84,7 +123,8 @@ class Node:
                 wrapRightState = np.copy(self.state)
                 wrapRightState[position[0]][col-1], wrapRightState[position[0]][position[1]] = \
                     wrapRightState[position[0]][position[1]], wrapRightState[position[0]][col-1]
-                wrapRightNode = Node(wrapRightState, self, self.weight + 2)
+                wrapRightNode = Node(wrapRightState, self, wrapRightState[position[0]][position[1]], 2, self.weight + 2, 0) if isUniformCost \
+                    else Node(wrapRightState, self, wrapRightState[position[0]][position[1]], 2, self.weight + 2)
                 alreadyExists = False
                 for successorNode in successorList:
                     if np.array_equal(wrapRightNode.state, successorNode.state):
@@ -96,7 +136,8 @@ class Node:
                 wrapTopState = np.copy(self.state)
                 wrapTopState[0][position[1]], wrapTopState[position[0]][position[1]] = \
                     wrapTopState[position[0]][position[1]], wrapTopState[0][position[1]]
-                wrapTopNode = Node(wrapTopState, self, self.weight + 2)
+                wrapTopNode = Node(wrapTopState, self, wrapTopState[position[0]][position[1]], 2, self.weight + 2, 0) if isUniformCost \
+                    else Node(wrapTopState, self, wrapTopState[position[0]][position[1]], 2, self.weight + 2)
                 alreadyExists = False
                 for successorNode in successorList:
                     if np.array_equal(wrapTopNode.state, successorNode.state):
@@ -107,8 +148,9 @@ class Node:
             elif position[0] == 0:
                 wrapBottomState = np.copy(self.state)
                 wrapBottomState[row - 1][position[1]], wrapBottomState[position[0]][position[1]] = \
-                    wrapBottomState[position[0]][position[1]], wrapBottomState[row -1 ][position[1]]
-                wrapBottomNode = Node(wrapBottomState, self, self.weight + 2)
+                    wrapBottomState[position[0]][position[1]], wrapBottomState[row -1][position[1]]
+                wrapBottomNode = Node(wrapBottomState, self, wrapBottomState[position[0]][position[1]], 2, self.weight + 2, 0) if isUniformCost \
+                    else Node(wrapBottomState, self, wrapBottomState[position[0]][position[1]], 2, self.weight + 2)
                 alreadyExists = False
                 for successorNode in successorList:
                     if np.array_equal(wrapBottomNode.state, successorNode.state):
@@ -121,86 +163,57 @@ class Node:
                 oppositeCornerState = np.copy(self.state)
                 oppositeCornerState[row-1][col-1], oppositeCornerState[0][0] = \
                 oppositeCornerState[0][0], oppositeCornerState[row-1][col-1]
-                oppositeCornerNode = Node(oppositeCornerState, self, self.weight + 3)
+                oppositeCornerNode = Node(oppositeCornerState, self, oppositeCornerState[0][0], 3, self.weight + 3, 0) if isUniformCost \
+                    else Node(oppositeCornerState, self, oppositeCornerState[0][0], 3, self.weight + 3)
                 successorList.append(oppositeCornerNode)
 
                 diagonalState = np.copy(self.state)
                 diagonalState[1][1], diagonalState[0][0] = \
                     diagonalState[0][0], diagonalState[1][1]
-                diagonalNode = Node(diagonalState, self, self.weight + 3)
+                diagonalNode = Node(diagonalState, self, diagonalState[0][0], 3, self.weight + 3, 0) if isUniformCost \
+                    else Node(diagonalState, self, diagonalState[0][0], 3, self.weight + 3)
                 successorList.append(diagonalNode)
             elif position == (0, col-1):
                 oppositeCornerState = np.copy(self.state)
                 oppositeCornerState[row - 1][0], oppositeCornerState[0][col-1] = \
                     oppositeCornerState[0][col-1], oppositeCornerState[row - 1][0]
-                oppositeCornerNode = Node(oppositeCornerState, self, self.weight + 3)
+                oppositeCornerNode = Node(oppositeCornerState, self, oppositeCornerState[0][col-1], 3, self.weight + 3, 0) if isUniformCost \
+                    else Node(oppositeCornerState, self, oppositeCornerState[0][col-1], 3, self.weight + 3)
                 successorList.append(oppositeCornerNode)
 
                 diagonalState = np.copy(self.state)
                 diagonalState[1][col-2], diagonalState[0][col-1] = \
                     diagonalState[0][col-1], diagonalState[1][col-2]
-                diagonalNode = Node(diagonalState, self, self.weight + 3)
+                diagonalNode = Node(diagonalState, self, diagonalState[0][col-1], 3, self.weight + 3, 0) if isUniformCost \
+                    else Node(diagonalState, self, diagonalState[0][col-1], 3, self.weight + 3)
                 successorList.append(diagonalNode)
             elif position == (row-1, 0):
                 oppositeCornerState = np.copy(self.state)
                 oppositeCornerState[0][col - 1], oppositeCornerState[row-1][0] = \
                     oppositeCornerState[row-1][0], oppositeCornerState[0][col - 1]
-                oppositeCornerNode = Node(oppositeCornerState, self, self.weight + 3)
+                oppositeCornerNode = Node(oppositeCornerState, self, oppositeCornerState[row-1][0], 3, self.weight + 3, 0) if isUniformCost \
+                    else Node(oppositeCornerState, self, oppositeCornerState[row-1][0], 3, self.weight + 3)
                 successorList.append(oppositeCornerNode)
 
                 diagonalState = np.copy(self.state)
                 diagonalState[row-2][1], diagonalState[row-1][0] = \
                     diagonalState[row-1][0], diagonalState[row-2][1]
-                diagonalNode = Node(diagonalState, self, self.weight + 3)
+                diagonalNode = Node(diagonalState, self, diagonalState[row-1][0], 3, self.weight + 3, 0) if isUniformCost \
+                    else Node(diagonalState, self, diagonalState[row-1][0], 3, self.weight + 3)
                 successorList.append(diagonalNode)
             elif position == (row-1, col-1):
                 oppositeCornerState = np.copy(self.state)
-                oppositeCornerState[row - 1][col - 1], oppositeCornerState[0][0] = \
-                    oppositeCornerState[0][0], oppositeCornerState[row - 1][col - 1]
-                oppositeCornerNode = Node(oppositeCornerState, self, self.weight + 3)
+                oppositeCornerState[0][0], oppositeCornerState[row - 1][col - 1] = \
+                    oppositeCornerState[row - 1][col - 1], oppositeCornerState[0][0]
+                oppositeCornerNode = Node(oppositeCornerState, self, oppositeCornerState[row - 1][col - 1], 3, self.weight + 3, 0) if isUniformCost \
+                    else Node(oppositeCornerState, self, oppositeCornerState[row - 1][col - 1], 3, self.weight + 3)
                 successorList.append(oppositeCornerNode)
 
                 diagonalState = np.copy(self.state)
                 diagonalState[row-2][col-2], diagonalState[row-1][col-1] = \
                     diagonalState[row-1][col-1], diagonalState[row-2][col-2]
-                diagonalNode = Node(diagonalState, self, self.weight + 3)
+                diagonalNode = Node(diagonalState, self, diagonalState[row-1][col-1], 3, self.weight + 3, 0) if isUniformCost \
+                    else Node(diagonalState, self, diagonalState[row-1][col-1], 3, self.weight + 3)
                 successorList.append(diagonalNode)
 
         return successorList
-
-
-
-# Parse string to get inital state puzzle
-initialState = Node(np.array([[1, 6, 3, 4], [0, 2, 7, 5]]), None, 0)
-
-openList = [initialState]
-closedList = []
-
-while True:
-    # Check if first Node is the goal
-    if openList[0].isGoal():
-        print("Solution found, Total cost: ", openList[0].weight)
-        break
-
-    # Pop the visited node and add to closed list
-    visitedNode = openList.pop(0)
-    closedList.append(visitedNode)
-
-    #Create successor
-    newSuccessorList = visitedNode.createSuccessors()
-    for successorNode in newSuccessorList:
-        for openNode in openList:
-            if np.array_equal(successorNode.state, openNode.state) and successorNode.weight < openNode.weight:
-                openList.remove(openNode)
-        alreadyExists = False
-        for closedNode in closedList:
-            if np.array_equal(successorNode.state, closedNode.state):
-                alreadyExists = True
-        if not alreadyExists:
-            openList.append(successorNode)
-
-    # Sort open list by weight
-    openList.sort(key=attrgetter('weight'))
-
-
-
---
+++ UniformCost.py
@@ -0,0 +1,73 @@
+import numpy as np
+import time
+from operator import attrgetter
+from Node import Node
+
+
+# Parse string to get inital state puzzle
+file1 = open('input.txt', 'r')
+Lines = file1.readlines()
+
+puzzleNumber = 0
+for line in Lines:
+    arr = []
+    for number in line.split():
+        arr.append(int(number))
+
+    initialState = Node(np.array([[arr[0], arr[1], arr[2], arr[3]], [arr[4], arr[5], arr[6], arr[7]]]), None, 0, 0, 0, 0)
+
+    openList = [initialState]
+    closedList = []
+    # Create Files
+    solutionFileName = str(puzzleNumber) + "_ucs_solution.txt"
+    searchFileName = str(puzzleNumber) + "_ucs_search.txt"
+
+    with open(searchFileName, mode='w+', newline='') as output_file:
+        start = time.time()
+        while True:
+            output_file.write("0 " + str(openList[0].weight) + " 0 " + openList[0].toString()+ "\n")
+            # Check if first Node is the goal
+            solutionFound = openList[0].isGoal()
+            if solutionFound:
+                end = time.time()
+                print("Solution found, Total cost: ", openList[0].weight)
+                break
+            if(time.time() - start) >= 60:
+                print("no solution")
+                break
+
+            # Pop the visited node and add to closed list
+            visitedNode = openList.pop(0)
+            closedList.append(visitedNode)
+
+            #Create successor
+            newSuccessorList = visitedNode.createSuccessors(True)
+            for successorNode in newSuccessorList:
+                for openNode in openList:
+                    if np.array_equal(successorNode.state, openNode.state) and successorNode.weight < openNode.weight:
+                        openList.remove(openNode)
+                alreadyExists = False
+                for closedNode in closedList:
+                    if np.array_equal(successorNode.state, closedNode.state):
+                        alreadyExists = True
+                if not alreadyExists:
+                    openList.append(successorNode)
+
+            # Sort open list by weight
+            openList.sort(key=attrgetter('weight'))
+    output_file.close()
+
+    # Add solution stuff
+    if solutionFound:
+        with open(solutionFileName, mode='w+', newline='') as output_file:
+            output_file.write(openList[0].solutionToString())
+            output_file.write(str(openList[0].weight) + " " + str(round(end-start, 3)))
+            output_file.close()
+    else:
+        with open(searchFileName, mode='w+', newline='') as output_file:
+            output_file.write("No solution")
+            output_file.close()
+        with open(solutionFileName, mode='w+', newline='') as output_file:
+            output_file.write("No solution")
+            output_file.close()
+    puzzleNumber += 1
